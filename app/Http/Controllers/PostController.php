1b673f1d8834dad0d1804b9d5c9749c7cf563a8c
kienpham2004 <phamkien20041998@gmail.com>
2021-04-02 17:37:39 +0700

    ok

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,8 +13,22 @@ class PostController extends Controller
 {
     public function getall(){
 
-        $data = DB::table('users')->get();
+//        $a = DB::table('users')->where('user_name', 'kienpham')->get();
+//        foreach ($a as $item){
+//            echo $item->email;
+//        }
 
+//        $users = DB::table('users')->pluck('email', 'user_name' );
+//        foreach ($users as $key=>$item){
+//            echo $key;
+//            echo '</br>';
+//        }
+//        $query = DB::table('users')->select('user_name');
+//        $users = $query->addSelect('phone')->get();
+//        dd($users);
+
+        $users = DB::table('users')->leftJoin('posts', 'users.id', '=', 'posts.user_id')->get();
+        dd($users);
     }
 }
 
